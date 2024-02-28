0371f13f91c7c8c7cb6b7e98c86bd91267a4b961
TBHGodPro <tarun.bhaskar@outlook.com>
2024-02-27 18:41:27 -0600

    Control hub detector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Managers/HubManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Managers/HubManager.java
@@ -2,15 +2,34 @@ package org.firstinspires.ftc.teamcode.common.Managers;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.configuration.LynxConstants;
 
 import java.util.List;
 import java.util.function.Consumer;
 
+import javax.annotation.Nullable;
+
 public class HubManager {
     public final List<LynxModule> hubs;
+    public final LynxModule controlHub;
+    @Nullable
+    public LynxModule expansionHub;
 
-    public HubManager(HardwareMap hardwareMap) {
+    public HubManager(HardwareMap hardwareMap) throws Exception {
         this.hubs = hardwareMap.getAll(LynxModule.class);
+
+        int numHubs = this.hubs.size();
+        if (numHubs < 1 || numHubs > 2) throw new Exception("Must have either 1 or 2 hubs.");
+        else if (numHubs == 1) {
+            controlHub = this.hubs.get(0);
+            expansionHub = null;
+        } else if (hubs.get(0).isParent() && LynxConstants.isEmbeddedSerialNumber(hubs.get(0).getSerialNumber())) {
+            controlHub = this.hubs.get(0);
+            expansionHub = this.hubs.get(1);
+        } else {
+            controlHub = this.hubs.get(1);
+            expansionHub = this.hubs.get(0);
+        }
     }
 
     public void runOnEachHub(Consumer<LynxModule> func) {
